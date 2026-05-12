c0410c209ab49ff07df63d6f061ee0969035f083
me <you@example.com>
2026-05-12 09:28:35 +0800

    初始提交：移动端 function call 工具定义及训练数据集

---
+++ tools_definition.py
@@ -0,0 +1,576 @@
+TOOLS = [
+    # === 应用管理 ===
+    {
+        "function": {
+            "name": "list_application",
+            "description": "列出此手机上安装的应用程序。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {},
+                "required": []
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "open_application",
+            "description": "打开指定应用程序。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "application_name": {
+                        "type": "STRING",
+                        "description": "要打开的应用程序名称。"
+                    }
+                },
+                "required": ["application_name"]
+            }
+        }
+    },
+
+    # === 显示控制 ===
+    {
+        "function": {
+            "name": "set_brightness",
+            "description": "设置当前屏幕亮度到指定级别。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "level": {
+                        "type": "STRING",
+                        "description": "亮度级别，范围 0 到 100。"
+                    }
+                },
+                "required": ["level"]
+            }
+        }
+    },
+
+    # === 通信 ===
+    {
+        "function": {
+            "name": "send_email",
+            "description": "发送电子邮件。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "contact_name": {
+                        "type": "STRING",
+                        "description": "收件人姓名。"
+                    },
+                    "subject": {
+                        "type": "STRING",
+                        "description": "邮件主题。"
+                    },
+                    "body": {
+                        "type": "STRING",
+                        "description": "邮件正文内容。"
+                    }
+                },
+                "required": ["contact_name", "subject"]
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "phone_call",
+            "description": "拨打指定联系人的电话。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "contact_name": {
+                        "type": "STRING",
+                        "description": "要拨打的联系人姓名。"
+                    }
+                },
+                "required": ["contact_name"]
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "phone_sms",
+            "description": "发送短信。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "contact_name": {
+                        "type": "STRING",
+                        "description": "短信收件人的姓名。"
+                    },
+                    "sms_message": {
+                        "type": "STRING",
+                        "description": "要发送的短信内容。"
+                    }
+                },
+                "required": ["contact_name"]
+            }
+        }
+    },
+
+    # === 系统信息 ===
+    {
+        "function": {
+            "name": "battery_status",
+            "description": "提供设备电池的相关信息。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {},
+                "required": []
+            }
+        }
+    },
+
+    # === 连接与网络 ===
+    {
+        "function": {
+            "name": "toggle_wifi",
+            "description": "开启或关闭Wi-Fi功能。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "enable": {
+                        "type": "BOOLEAN",
+                        "description": "true 为开启 Wi-Fi，false 为关闭 Wi-Fi。"
+                    }
+                },
+                "required": ["enable"]
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "toggle_bluetooth",
+            "description": "开启或关闭蓝牙功能。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "enable": {
+                        "type": "BOOLEAN",
+                        "description": "true 为开启蓝牙，false 为关闭蓝牙。"
+                    }
+                },
+                "required": ["enable"]
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "toggle_mobile_data",
+            "description": "开启或关闭移动数据网络。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "enable": {
+                        "type": "BOOLEAN",
+                        "description": "true 为开启移动数据，false 为关闭移动数据。"
+                    }
+                },
+                "required": ["enable"]
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "toggle_airplane_mode",
+            "description": "开启或关闭飞行模式。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "enable": {
+                        "type": "BOOLEAN",
+                        "description": "true 为开启飞行模式，false 为关闭飞行模式。"
+                    }
+                },
+                "required": ["enable"]
+            }
+        }
+    },
+
+    # === 媒体 ===
+    {
+        "function": {
+            "name": "play_music",
+            "description": "播放音乐或歌曲。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "song_name": {
+                        "type": "STRING",
+                        "description": "要播放的歌曲名称。"
+                    },
+                    "artist": {
+                        "type": "STRING",
+                        "description": "歌手名称。"
+                    }
+                },
+                "required": []
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "pause_music",
+            "description": "暂停当前播放的音乐。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {},
+                "required": []
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "take_picture",
+            "description": "使用相机拍照。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {},
+                "required": []
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "take_screenshot",
+            "description": "截取当前屏幕内容。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {},
+                "required": []
+            }
+        }
+    },
+
+    # === 硬件控制 ===
+    {
+        "function": {
+            "name": "set_flashlight",
+            "description": "开启或关闭手电筒。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "enable": {
+                        "type": "BOOLEAN",
+                        "description": "true 为打开手电筒，false 为关闭手电筒。"
+                    }
+                },
+                "required": ["enable"]
+            }
+        }
+    },
+
+    # === 地图与定位 ===
+    {
+        "function": {
+            "name": "show_map",
+            "description": "在地图上显示位置。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "query": {
+                        "type": "STRING",
+                        "description": "要搜索的位置，可以是地点名称、商家或地址。"
+                    }
+                },
+                "required": ["query"]
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "get_location",
+            "description": "获取当前设备地理位置。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {},
+                "required": []
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "navigate",
+            "description": "启动导航到指定目的地。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "destination": {
+                        "type": "STRING",
+                        "description": "导航目的地，可以是地址、地点名称或坐标。"
+                    }
+                },
+                "required": ["destination"]
+            }
+        }
+    },
+
+    # === 通讯录 ===
+    {
+        "function": {
+            "name": "create_contact",
+            "description": "创建联系人。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "contact_name": {
+                        "type": "STRING",
+                        "description": "联系人姓名。"
+                    },
+                    "email": {
+                        "type": "STRING",
+                        "description": "联系人的电子邮件地址。"
+                    },
+                    "phone_number": {
+                        "type": "STRING",
+                        "description": "联系人的电话号码。"
+                    }
+                },
+                "required": ["contact_name"]
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "search_contacts",
+            "description": "搜索联系人。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "contact_name": {
+                        "type": "STRING",
+                        "description": "要搜索的联系人姓名。"
+                    }
+                },
+                "required": ["contact_name"]
+            }
+        }
+    },
+
+    # === 提醒与时间 ===
+    {
+        "function": {
+            "name": "set_alarm",
+            "description": "设置一个闹钟。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "datetime": {
+                        "type": "STRING",
+                        "description": "闹钟的日期和时间，格式为 YYYY-MM-DDTHH:MM:SS。"
+                    },
+                    "label": {
+                        "type": "STRING",
+                        "description": "闹钟标签/备注。"
+                    },
+                    "repeat": {
+                        "type": "STRING",
+                        "description": "重复模式，如 'daily'、'weekly'、'weekdays' 等。"
+                    }
+                },
+                "required": ["datetime"]
+            }
+        }
+    },
+
+    # === 声音控制 ===
+    {
+        "function": {
+            "name": "set_volume",
+            "description": "设置设备音量。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "level": {
+                        "type": "STRING",
+                        "description": "音量级别，范围 0 到 100。"
+                    }
+                },
+                "required": ["level"]
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "set_mute",
+            "description": "设置设备静音状态。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "enable": {
+                        "type": "BOOLEAN",
+                        "description": "true 为静音，false 为取消静音。"
+                    }
+                },
+                "required": ["enable"]
+            }
+        }
+    },
+
+    # === 浏览与搜索 ===
+    {
+        "function": {
+            "name": "open_browser",
+            "description": "打开浏览器访问指定网址。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "url": {
+                        "type": "STRING",
+                        "description": "要打开的网址。"
+                    }
+                },
+                "required": ["url"]
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "search_web",
+            "description": "使用搜索引擎进行搜索。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "query": {
+                        "type": "STRING",
+                        "description": "搜索关键词。"
+                    }
+                },
+                "required": ["query"]
+            }
+        }
+    },
+
+    # === 系统模式 ===
+    {
+        "function": {
+            "name": "toggle_dnd",
+            "description": "开启或关闭勿扰模式。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "enable": {
+                        "type": "BOOLEAN",
+                        "description": "true 为开启勿扰模式，false 为关闭勿扰模式。"
+                    }
+                },
+                "required": ["enable"]
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "toggle_power_saving_mode",
+            "description": "开启或关闭省电模式。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "enable": {
+                        "type": "BOOLEAN",
+                        "description": "true 为开启省电模式，false 为关闭省电模式。"
+                    }
+                },
+                "required": ["enable"]
+            }
+        }
+    },
+
+    # === 天气服务 ===
+    {
+        "function": {
+            "name": "get_weather",
+            "description": "获取天气信息。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "city": {
+                        "type": "STRING",
+                        "description": "要查询天气的城市名称。不提供则查询当前位置天气。"
+                    }
+                },
+                "required": []
+            }
+        }
+    },
+
+    # === 日程管理 ===
+    {
+        "function": {
+            "name": "create_calendar_event",
+            "description": "创建一个新的日历事件。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "title": {
+                        "type": "STRING",
+                        "description": "事件标题。"
+                    },
+                    "datetime": {
+                        "type": "STRING",
+                        "description": "事件的日期和时间，格式为 YYYY-MM-DDTHH:MM:SS。"
+                    }
+                },
+                "required": ["title", "datetime"]
+            }
+        }
+    },
+    {
+        "function": {
+            "name": "search_calendar_events",
+            "description": "查询日历中的事件。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "query": {
+                        "type": "STRING",
+                        "description": "搜索关键词，按事件标题匹配。不提供则返回最近的事件列表。"
+                    },
+                    "start_date": {
+                        "type": "STRING",
+                        "description": "查询起始日期，格式为 YYYY-MM-DD。"
+                    },
+                    "end_date": {
+                        "type": "STRING",
+                        "description": "查询结束日期，格式为 YYYY-MM-DD。"
+                    }
+                },
+                "required": []
+            }
+        }
+    },
+
+    # === 备忘录 ===
+    {
+        "function": {
+            "name": "create_note",
+            "description": "创建一条备忘录或笔记。",
+            "parameters": {
+                "type": "OBJECT",
+                "properties": {
+                    "title": {
+                        "type": "STRING",
+                        "description": "备忘录标题。"
+                    },
+                    "content": {
+                        "type": "STRING",
+                        "description": "备忘录内容。"
+                    }
+                },
+                "required": ["title", "content"]
+            }
+        }
+    },
+]
+
+
+def get_tool_by_name(name: str) -> dict | None:
+    """按名称获取单个工具定义。"""
+    for tool in TOOLS:
+        if tool["function"]["name"] == name:
+            return tool
+    return None
+
+
+def get_tool_names() -> list[str]:
+    """获取所有工具名称列表。"""
+    return [t["function"]["name"] for t in TOOLS]
---
+++ tools_table.md
@@ -0,0 +1,34 @@
+| 索引 | 工具名称 | 分类 | 描述 | 必选参数 | 可选参数 |
+| -- | --- | --- | --- | --- | --- |
+| 1 | list_application | 应用管理 | 列出此手机上安装的应用程序。 | 无 | 无 |
+| 2 | open_application | 应用管理 | 打开指定应用程序。 | application_name | 无 |
+| 3 | set_brightness | 显示控制 | 设置当前屏幕亮度到指定级别。 | level | 无 |
+| 4 | send_email | 通信 | 发送电子邮件。 | contact_name, subject | body |
+| 5 | phone_call | 通信 | 拨打指定联系人的电话。 | contact_name | 无 |
+| 6 | phone_sms | 通信 | 发送短信。 | contact_name | sms_message |
+| 7 | battery_status | 系统信息 | 提供设备电池的相关信息。 | 无 | 无 |
+| 8 | toggle_wifi | 连接与网络 | 开启或关闭Wi-Fi功能。 | enable | 无 |
+| 9 | toggle_bluetooth | 连接与网络 | 开启或关闭蓝牙功能。 | enable | 无 |
+| 10 | toggle_mobile_data | 连接与网络 | 开启或关闭移动数据网络。 | enable | 无 |
+| 11 | toggle_airplane_mode | 连接与网络 | 开启或关闭飞行模式。 | enable | 无 |
+| 12 | play_music | 媒体 | 播放音乐或歌曲。 | 无 | song_name, artist |
+| 13 | pause_music | 媒体 | 暂停当前播放的音乐。 | 无 | 无 |
+| 14 | take_picture | 媒体 | 使用相机拍照。 | 无 | 无 |
+| 15 | take_screenshot | 媒体 | 截取当前屏幕内容。 | 无 | 无 |
+| 16 | set_flashlight | 硬件控制 | 开启或关闭手电筒。 | enable | 无 |
+| 17 | show_map | 地图与定位 | 在地图上显示位置。 | query | 无 |
+| 18 | get_location | 地图与定位 | 获取当前设备地理位置。 | 无 | 无 |
+| 19 | navigate | 地图与定位 | 启动导航到指定目的地。 | destination | 无 |
+| 20 | create_contact | 通讯录 | 创建联系人。 | contact_name | email, phone_number |
+| 21 | search_contacts | 通讯录 | 搜索联系人。 | contact_name | 无 |
+| 22 | set_alarm | 提醒与时间 | 设置一个闹钟。 | datetime | label, repeat |
+| 23 | set_volume | 声音控制 | 设置设备音量。 | level | 无 |
+| 24 | set_mute | 声音控制 | 设置设备静音状态。 | enable | 无 |
+| 25 | open_browser | 浏览与搜索 | 打开浏览器访问指定网址。 | url | 无 |
+| 26 | search_web | 浏览与搜索 | 使用搜索引擎进行搜索。 | query | 无 |
+| 27 | toggle_dnd | 系统模式 | 开启或关闭勿扰模式。 | enable | 无 |
+| 28 | toggle_power_saving_mode | 系统模式 | 开启或关闭省电模式。 | enable | 无 |
+| 29 | get_weather | 天气服务 | 获取天气信息。 | 无 | city |
+| 30 | create_calendar_event | 日程管理 | 创建一个新的日历事件。 | title, datetime | 无 |
+| 31 | search_calendar_events | 日程管理 | 查询日历中的事件。 | 无 | query, start_date, end_date |
+| 32 | create_note | 备忘录 | 创建一条备忘录或笔记。 | title, content | 无 |
